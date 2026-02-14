a91f5bda921d0dbe29d9d9c9850178cb4d1d501e
MUNJAM PRAVEEN KUMAR <160229543+Munjampraveen143@users.noreply.github.com>
2026-02-14 11:27:28 +0530

    Add SQL queries for database exploration and insights
    This SQL script explores the database and retrieves various insights such as unique marital statuses, genders, countries, customer age distribution, and revenue generation metrics.

---
+++ SCRIPTS/EDA/Queries.sql
@@ -0,0 +1,74 @@
+-- exploring database
+
+select * from INFORMATION_SCHEMA.tables;
+select * from INFORMATION_SCHEMA.columns;
+
+
+select distinct marital_status from gold.dim_customers;
+select distinct gender from gold.dim_customers;
+
+-- retrieve list of unique countries
+select distinct country from gold.dim_customers;
+
+-- show the first and last order and total duration in months
+select min(order_date) as first_order, max(order_date) as last_order,
+datediff(month,min(order_date), max(order_date)) as month_differ
+from gold.fact_sales;
+
+
+-- find the youngest and oldest customers and age
+select min(birthdate) as oldest_customer, max(birthdate) as youngest_customer,
+datediff(year,min(birthdate),getdate()) as old_age,
+datediff(year,max(birthdate),getdate()) as young_age
+from gold.dim_customers;  
+
+-- total customers by country
+select country,count(customer_key) as total_customers
+from gold.dim_customers
+group by country
+order by count(customer_key) desc;
+
+-- find total customer by gender
+select gender,count(customer_key) as total_customers
+from gold.dim_customers
+group by gender
+order by count(customer_key) desc;
+
+-- find total products by category
+select category,count(product_key) as total_products
+from gold.dim_products
+group by category
+order by count(product_key) desc;
+
+
+-- what is the avg total revenue genarated by each category
+select p.category as product_category,
+avg(p.cost) as avg_amount,
+sum(f.sales_amount) as total_revenue
+from gold.dim_products as p
+left join gold.fact_sales as f
+on p.product_key = f.product_key
+group by p.category
+order by  total_revenue desc;
+
+
+-- what is the total revenue genarated by each customer
+select c.customer_key,
+c.first_name,
+sum(f.sales_amount) as total_revenue
+from gold.dim_customers as c
+left join gold.fact_sales as f
+on c.customer_key = f.customer_key
+group by c.customer_key,
+c.first_name
+order by total_revenue desc;
+
+
+-- what is  the distribution of sold items across countries
+select c.country,
+sum(f.quantity) as total_sold
+from gold.dim_customers as c
+left join gold.fact_sales as f
+on c.customer_key = f.customer_key
+group by c.country
+order by total_sold desc;
